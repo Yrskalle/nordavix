ede6f58323a417f7ff7457f4579d6b9be83c3ce6
Jonas Holm <jonas.holm1979@gmail.com>
2025-12-10 11:15:39 +0100

    First commit

---
+++ app/main/routes.py
@@ -0,0 +1,18 @@
+from flask import render_template
+from app.main import main_bp
+
+@main_bp.route('/')
+def index():
+    return render_template('main/index.html')
+
+@main_bp.route('/services')
+def services():
+    return render_template('main/services.html')
+
+@main_bp.route('/about')
+def about():
+    return render_template('main/about.html')
+
+@main_bp.route('/contact')
+def contact():
+    return render_template('main/contact.html')
