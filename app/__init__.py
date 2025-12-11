66d03f0b3c314847f218e1f7264af0db90de6e5c
Jonas Holm <jonas.holm1979@gmail.com>
2025-12-10 23:43:32 -0500

    First draft

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,7 @@
 from flask import Flask
+from datetime import datetime
+
+
 
 def create_app():
     app = Flask(__name__)
@@ -6,5 +9,11 @@ def create_app():
     # Importera och registrera blueprint
     from app.main import main_bp
     app.register_blueprint(main_bp)
+
+    @app.context_processor
+    def inject_globals():
+        return {
+            "current_year": datetime.now().year
+        }
 
     return app
